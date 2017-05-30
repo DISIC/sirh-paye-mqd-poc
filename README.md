cc9d541d08bb1a41a15d2d9c3b4dc08be14c9ca5
atingaud <atingaud@octo.com>
2017-05-30 19:47:08 +0200

    [ATI] ETQ DEV j'automatise les déploiements sur l'environnement de Démo

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # sirh-paye-mqd-poc
+
+Prérequis
+=====================
+
+* Java 8
+* Maven 3
+* Tomcat 8
+
+Environnement de Démo
+=====================
+
+Configurations
+--------------------
+
+* Chemin JAVA_HOME : /usr/java/jdk1.8.0_131
+* Chemin TOMCAT_HOME : /opt/tomcat
+* Utilisateur du process Tomcat : tomcat
+* Groupe du process Tomcat : tomcat
+* Récupérer le script **livraison-projet.sh** situé à la racine du projet
+
+Installation
+--------------------
+
+**Command** : ./livraison-projet.sh <RELEASE-GIT AU FORMAT : X.Y.Z>
+
+N.B. : La liste des RELEASES GIT est disponible à l'adresse suivante :
+https://github.com/DISIC/sirh-paye-mqd-poc/releases/
+
+Accès aux Services
+--------------------
+
+**IHM Reporting** : http://10.200.54.183:8080/reporting/
+
+**Web Services Supplier** : http://10.200.54.183:8080/supplier/...
---
+++ mqd-root-project/parent/livraison-projet.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+######################################################################################
+# Program: livraison-projet.sh
+#               Copyright OCTO Technology 2017
+#
+# Description:
+#       Ce script permet d'automatiser la livraison des livrables sur l'environnement de démo fourni par les Ministères Sociaux.
+# 
+# Pré-requis:
+#       - S'assurer que l'utilisateur qui exécute ce script ait des droits d'écriture dans le répertoire courant.
+#       - Si nécessaire rendre le script executable: chmod +x livraison-projet.sh
+#       - Aria2c, ZIP, Maven, Java et Tomcat doivent être installés sur la VM cible
+#       - Ce script doit être déployé sur la VM
+#
+#  Lancer les étapes une à une (où $VERSION est la version du projet à installer):
+#       1 - ./livraison-projet.sh $VERSION	(exécution de la procédure)
+#
+######################################################################################
+# Auteur:   Alexandre TINGAUD (atingaud@octo.com)
+#
+# Date de creation: 30/05/2017
+#
+# Dernieres Modifications:
+#
+# 30/05/2017	Version: 1.0
+#       Original Code
+######################################################################################
+
+LOCATION=$(pwd)
+VERSION=$1
+
+######################################################################################
+# Informations sur l'emplacement actuel de l'utilisateur
+######################################################################################
+check_current_location() {
+	echo -e "Emplacement actuel : $LOCATION."
+}
+
+######################################################################################
+# Suppression de l'ancienne version du projet
+######################################################################################
+cleanup_project() {
+	echo -e "Suppression de l'ancienne version du projet."
+	sudo rm -Rf ./sirh-paye-mqd-poc-tag-*
+}
+
+######################################################################################
+# Téléchargement de la version du code source du projet
+######################################################################################
+scripts_git_tag() {
+	echo -e "Téléchargement de la version $VERSION du code source du projet"
+	aria2c -x10 "https://github.com/DISIC/sirh-paye-mqd-poc/archive/tag-$VERSION.zip"
+	unzip "tag-$VERSION.zip" -d "$LOCATION"
+}
+
+######################################################################################
+# Génération des configurations du projet propres à l'environnement de Démo
+######################################################################################
+scripts_mvn_install() {
+	echo -e "Génération des configurations du projet propres à l'environnement de Démo"
+	cd "$LOCATION/sirh-paye-mqd-poc-tag-$VERSION/mqd-root-project/parent"
+	mvn clean install -p demo
+}
+
+######################################################################################
+# Installation du projet reporting
+######################################################################################
+scripts_reporting_delivery() {
+	echo -e "Installation du projet reporting"
+	cd "$LOCATION/sirh-paye-mqd-poc-tag-$VERSION/mqd-root-project/reporting/target"
+	sudo chmod +x "./livraison-projet-reporting.sh"
+	sh ./livraison-projet-reporting.sh $VERSION
+}
+
+######################################################################################
+# Installation du projet supplier
+######################################################################################
+scripts_supplier_delivery() {
+	echo -e "Installation du projet supplier"
+	cd "$LOCATION/sirh-paye-mqd-poc-tag-$VERSION/mqd-root-project/supplier/target"
+	sudo chmod +x "./livraison-projet-supplier.sh"
+	sh ./livraison-projet-supplier.sh $VERSION
+}
+
+check_current_location
+cleanup_project
+scripts_git_tag
+scripts_mvn_install
+scripts_reporting_delivery
