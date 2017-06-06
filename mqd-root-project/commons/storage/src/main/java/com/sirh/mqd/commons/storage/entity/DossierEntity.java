49b560b1f1d8022cb23f308a0b54140f43ad75f5
Khalil GHARBI <kgharbi@octo.com>
2017-06-06 11:17:35 +0200

    [KGH]: merged upgrade version

--- mqd-root-project/commons/storage/src/main/java/com/sirh/mqd/commons/storage/entity/DossierEntity.java
+++ mqd-root-project/commons/storage/src/main/java/com/sirh/mqd/commons/storage/entity/DossierEntity.java
@@ -20,10 +20,10 @@ import com.sirh.mqd.commons.storage.constantes.DossierConstantes;
  */
 @Document(collection = DossierConstantes.COLLECTION_NAME)
 @CompoundIndexes({
-		@CompoundIndex(name = "index_dossier", def = "{'" + DossierConstantes.COLONNE_PAY_LOT + "' : 1, '"
-				+ DossierConstantes.COLONNE_MATRICULE + "': 1}"),
-		@CompoundIndex(name = "index_user_profile_mse", def = "{'" + DossierConstantes.COLONNE_PAY_LOT + "' : 1, '"
-				+ DossierConstantes.COLONNE_CORPS_CODE + "': 1}") })
+	@CompoundIndex(name = "index_dossier", def = "{'" + DossierConstantes.COLONNE_PAY_LOT + "' : 1, '"
+			+ DossierConstantes.COLONNE_MATRICULE + "': 1}"),
+	@CompoundIndex(name = "index_user_profile_mse", def = "{'" + DossierConstantes.COLONNE_PAY_LOT + "' : 1, '"
+			+ DossierConstantes.COLONNE_CORPS_CODE + "': 1}") })
 public class DossierEntity {
 
 	@Id
@@ -76,187 +76,6 @@ public class DossierEntity {
 	private String renoiRHAffectationLibelleCourt;
 
 	@Field(DossierConstantes.COLONNE_CIVILITE)
-<<<<<<< HEAD
-	private int renoiRHCivilite;
-
-	@Field(DossierConstantes.COLONNE_NOM)
-	private String renoiRHNom;
-
-	@Field(DossierConstantes.COLONNE_PRENOM)
-	private String renoiRHPrenom;
-
-	@Field(DossierConstantes.COLONNE_SEXE)
-	private int renoiRHSexe;
-
-	@Field(DossierConstantes.COLONNE_DATE_NAISSANCE)
-	private Date renoiRHDateNaissance; // Format : "01/MM/yyyy"
-
-	@Field(DossierConstantes.COLONNE_PAY_DATE_CERTIFICATION)
-	private Date renoiRHDateCertification; // Format : "dd/MM/yyyy"
-
-	@Field(DossierConstantes.COLONNE_DATES_MOUVEMENTS_CARRIERE)
-	private List<Date> mouvementsCarriere; // Format : "dd/MM/yyyy"
-
-	@Transient
-	private int nbAlertes;
-
-	@Transient
-	private int nbAnomalies;
-
-	public DossierEntity() {
-		super();
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(final String id) {
-		this.id = id;
-	}
-
-	public int getMinistere() {
-		return ministere;
-	}
-
-	public void setMinistere(final int ministere) {
-		this.ministere = ministere;
-	}
-
-	public String getPayCle() {
-		return payCle;
-	}
-
-	public void setPayCle(final String payCle) {
-		this.payCle = payCle;
-	}
-
-	public int getDossierNumero() {
-		return dossierNumero;
-	}
-
-	public void setDossierNumero(final int dossierNumero) {
-		this.dossierNumero = dossierNumero;
-	}
-
-	public int getDiGestionnaire() {
-		return diGestionnaire;
-	}
-
-	public void setDiGestionnaire(final int diGestionnaire) {
-		this.diGestionnaire = diGestionnaire;
-	}
-
-	public String getAdminCode() {
-		return adminCode;
-	}
-
-	public void setAdminCode(final String adminCode) {
-		this.adminCode = adminCode;
-	}
-
-	public String getAdminCodeDepartement() {
-		return adminCodeDepartement;
-	}
-
-	public void setAdminCodeDepartement(final String adminCodeDepartement) {
-		this.adminCodeDepartement = adminCodeDepartement;
-	}
-
-	public String getPayLot() {
-		return payLot;
-	}
-
-	public void setPayLot(final String payLot) {
-		this.payLot = payLot;
-	}
-
-	public String getRenoiRHMatricule() {
-		return renoiRHMatricule;
-	}
-
-	public void setRenoiRHMatricule(final String renoiRHMatricule) {
-		this.renoiRHMatricule = renoiRHMatricule;
-	}
-
-	public String getRenoiRHCorpsCode() {
-		return renoiRHCorpsCode;
-	}
-
-	public void setRenoiRHCorpsCode(final String renoiRHCorpsCode) {
-		this.renoiRHCorpsCode = renoiRHCorpsCode;
-	}
-
-	public String getRenoiRHCorpsLibelleCourt() {
-		return renoiRHCorpsLibelleCourt;
-	}
-
-	public void setRenoiRHCorpsLibelleCourt(final String renoiRHCorpsLibelleCourt) {
-		this.renoiRHCorpsLibelleCourt = renoiRHCorpsLibelleCourt;
-	}
-
-	public String getRenoiRHGradeCode() {
-		return renoiRHGradeCode;
-	}
-
-	public void setRenoiRHGradeCode(final String renoiRHGradeCode) {
-		this.renoiRHGradeCode = renoiRHGradeCode;
-	}
-
-	public String getRenoiRHGradeLibelleCourt() {
-		return renoiRHGradeLibelleCourt;
-	}
-
-	public void setRenoiRHGradeLibelleCourt(final String renoiRHGradeLibelleCourt) {
-		this.renoiRHGradeLibelleCourt = renoiRHGradeLibelleCourt;
-	}
-
-	public String getRenoiRHAffectationCode() {
-		return renoiRHAffectationCode;
-	}
-
-	public void setRenoiRHAffectationCode(final String renoiRHAffectationCode) {
-		this.renoiRHAffectationCode = renoiRHAffectationCode;
-	}
-
-	public String getRenoiRHAffectationLibelleCourt() {
-		return renoiRHAffectationLibelleCourt;
-	}
-
-	public void setRenoiRHAffectationLibelleCourt(final String renoiRHAffectationLibelleCourt) {
-		this.renoiRHAffectationLibelleCourt = renoiRHAffectationLibelleCourt;
-	}
-
-	public int getRenoiRHCivilite() {
-		return renoiRHCivilite;
-	}
-
-	public void setRenoiRHCivilite(final int renoiRHCivilite) {
-		this.renoiRHCivilite = renoiRHCivilite;
-	}
-
-	public String getRenoiRHNom() {
-		return renoiRHNom;
-	}
-
-	public void setRenoiRHNom(final String renoiRHNom) {
-		this.renoiRHNom = renoiRHNom;
-	}
-
-	public String getRenoiRHPrenom() {
-		return renoiRHPrenom;
-	}
-
-	public void setRenoiRHPrenom(final String renoiRHPrenom) {
-		this.renoiRHPrenom = renoiRHPrenom;
-	}
-
-	public int getRenoiRHSexe() {
-		return renoiRHSexe;
-	}
-
-	public void setRenoiRHSexe(final int renoiRHSexe) {
-=======
 	private Integer renoiRHCivilite;
 
 	@Field(DossierConstantes.COLONNE_NOM)
@@ -436,7 +255,6 @@ public class DossierEntity {
 	}
 
 	public void setRenoiRHSexe(final Integer renoiRHSexe) {
->>>>>>> branch 'master' of https://github.com/DISIC/sirh-paye-mqd-poc.git
 		this.renoiRHSexe = renoiRHSexe;
 	}
 
