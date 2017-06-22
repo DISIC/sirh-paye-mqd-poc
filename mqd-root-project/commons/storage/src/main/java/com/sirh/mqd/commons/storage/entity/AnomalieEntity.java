4e7abfef0209a4204af88e331c173b62fa60e2ff
atingaud <atingaud@octo.com>
2017-06-22 10:27:31 +0200

    [ATI] Code review : Dans le tableau des anomalies, 2 colonnes sont en trop : Date clôture correction + Numéro anomalie - La date clôture doit être stockée en BDD + correction problème de compilation sur composant supplier

--- mqd-root-project/commons/storage/src/main/java/com/sirh/mqd/commons/storage/entity/AnomalieEntity.java
+++ mqd-root-project/commons/storage/src/main/java/com/sirh/mqd/commons/storage/entity/AnomalieEntity.java
@@ -20,6 +20,9 @@ public class AnomalieEntity {
 	@Field(ComparaisonConstantes.COLONNE_ANOMALIE_DATE_ECHEANCE)
 	private Date dateEcheance;
 
+	@Field(ComparaisonConstantes.COLONNE_ANOMALIE_DATE_CLOTURE)
+	private Date dateCloture;
+
 	@Field(ComparaisonConstantes.COLONNE_ANOMALIE_ETAT)
 	private AnomalieEtatEnum etatCorrection;
 
@@ -53,6 +56,14 @@ public class AnomalieEntity {
 
 	public void setDateEcheance(final Date dateEcheance) {
 		this.dateEcheance = dateEcheance;
+	}
+
+	public Date getDateCloture() {
+		return dateCloture;
+	}
+
+	public void setDateCloture(final Date dateCloture) {
+		this.dateCloture = dateCloture;
 	}
 
 	public AnomalieEtatEnum getEtatCorrection() {
